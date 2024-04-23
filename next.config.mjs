daf2d69b9502d33a83fed12d50a33cb95f29b8b1
wangzhongxi <wangzhongxi@meituan.com>
2024-04-23 10:46:44 +0800

    feat: remove stream-object route

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/about',
-        destination: '/',
-      },
-    ]
-  },
 };
 
 export default nextConfig;
